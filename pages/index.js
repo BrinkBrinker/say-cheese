09f04e6802b9d1616b9361d308fccd9f011a8635
BrinkBrinker <62677761+BrinkBrinker@users.noreply.github.com>
2022-04-13 22:39:17 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+       <img src=""components/free-mtn-dew-code-red-coupon.gif>
       </main>
 
       <Footer />
