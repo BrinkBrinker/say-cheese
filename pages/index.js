2c9712a29d377ecdb993e467a335c00944c67fca
BrinkBrinker <62677761+BrinkBrinker@users.noreply.github.com>
2022-04-13 22:48:15 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-       <img src="components/free-mtn-dew-code-red-coupon.gif" />
+       <img src="https://github.com/BrinkBrinker/say-cheese/blob/main/components/free%20mtn%20dew%20code%20red%20coupon.gif?raw=true" />
       </main>
 
       <Footer />
